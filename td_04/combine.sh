4c78295d08851c1668cb92099ff4b40e9d75c402
zhenyuefu <zhenyue.fu@etu.upmc.fr>
2020-10-28 10:26:02 +0000

    td3

---
+++ td_04/combine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+[ $# -ne 1 ] && echo "Il faut un param !" && exit 1
+[ ! -f $1 ] && echo "Il faut un fichier en param !" && exit 1
+
+tmp=$1.tmp
+echo $tmp
+
+./numerotation.sh < $1 > $tmp 
+./majuscule.sh < $tmp
