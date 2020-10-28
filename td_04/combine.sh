ff996f985ef3b1c785e4b1c3adcba9688851f646
zhenyuefu <zhenyue.fu@etu.upmc.fr>
2020-10-28 10:53:39 +0000

    4.5

--- td_04/combine.sh
+++ td_04/combine.sh
@@ -2,10 +2,12 @@
 [ $# -ne 1 ] && echo "Il faut un param !" && exit 1
 [ ! -f $1 ] && echo "Il faut un fichier en param !" && exit 1
 
-tmp=$1.tmp
-echo $tmp
+./numerotation.sh < $1 | ./majuscule.sh
 
-./numerotation.sh < $1 > $tmp 
-./majuscule.sh < $tmp
+# tmp=$1.tmp
+# echo $tmp
 
-rm -f $tmp
+# ./numerotation.sh < $1 > $tmp 
+# ./majuscule.sh < $tmp
+
+# rm -f $tmp
